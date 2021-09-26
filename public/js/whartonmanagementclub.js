2ceaabad4ef848eb2b07889fd95e42017f54a644
Charles Cunningham <mureytasroc@gmail.com>
2021-09-26 03:05:28 -0400

    Fix board members

--- public/js/whartonmanagementclub.js
+++ public/js/whartonmanagementclub.js
@@ -13,7 +13,8 @@ $('#'+titleToId[Object.keys(titleToId).reduce((e, r)=>{
   if(document.title.includes(e)){ return e; } else{ return r; }
 }, 'none')]).css('color','#EA4231')
 
-setTimeout(function(){ $.jsdvPopup({}); }, 500)
+// UNCOMMENT THIS FOR RECRUITING
+// setTimeout(function(){ $.jsdvPopup({}); }, 500)
 
 vp_amp = {
   "name": "Charley Cunningham",
@@ -24,11 +25,6 @@ vp_finance = {
   "name": "Lucia Macchi",
   "penn_key": "lmacchi",
   "domain": "wharton"
-}
-vp_relations = {
-  "name": "Matt Dieffenthaller",
-  "penn_key": "dieffmat",
-  "domain": "seas"
 }
 vp_sae = {
   "name": "Ryan Sachs",
@@ -45,11 +41,6 @@ $("#vp_finance_name").text(vp_finance["name"])
 email = vp_finance["penn_key"] + "@" + vp_finance["domain"] + ".upenn.edu"
 $("#vp_finance_email").text(email)
 $("#vp_finance_email")
-
-$("#vp_relations_name").text(vp_relations["name"])
-email = vp_relations["penn_key"] + "@" + vp_relations["domain"] + ".upenn.edu"
-$("#vp_relations_email").text(email)
-$("#vp_relations_email").attr("href", "mailto:" + email)
 
 $("#vp_sae_name").text(vp_sae["name"])
 email = vp_sae["penn_key"] + "@" + vp_sae["domain"] + ".upenn.edu"
